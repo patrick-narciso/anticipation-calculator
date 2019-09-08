8f5dba994a76c6e4a222fbd9a11ceb5ae1857a05
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-09-08 02:56:48 -0300

    feat: adiciona cor de fundo na aplicação e fonte Source Sans

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import { render } from '@/components/Route';
+import { injectGlobal } from 'emotion';
+import tokens from '@Uikit/Tokens';
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background: ${tokens.color('grey', 'dark')};
+    font-family: ${tokens.get('type.regularFontFamily')};
+  }
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap');
+`;
 
 render();
