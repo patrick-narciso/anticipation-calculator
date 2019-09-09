03f1923834ee2a77619e5faa696a8f2b51ed00f0
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-09-09 16:29:22 -0300

    feat: apresenta os valores de recebíveis retornados no tópico antecipationData na tela

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import { dom } from 'isomorphic-jsx';
 import { css } from 'emotion';
+import { validateForm, currencyToFloat, formatReal } from '@/utils/form';
 import { TextField } from '@Uikit';
 import tokens from '@Uikit/Tokens';
 import VMasker from 'vanilla-masker';
+import { getValueAntecipation } from '@receivables/calculator';
+import PubSub from 'PubSub';
+
+const pubsub = new PubSub();
 
 const Home = () => {
   return (
@@ -11,29 +16,32 @@ const Home = () => {
       <Box>
         <Simulator>
           <SimulatorTitle>Simule sua Antecipação</SimulatorTitle>
-          <TextField id="amount" type="text" label="Informe o valor da venda*" />
-          <TextField
-            id="installments"
-            extraMessage="Máximo de 12 parcelas"
-            type="number"
-            label="Em quantas parcelas*"
-          />
-          <TextField id="mdr" type="text" label="Informe o percentual de MDR*" />
+          <form id="form">
+            <TextField id="amount" type="text" required="required" label="Informe o valor da venda*" />
+            <TextField
+              id="installments"
+              extraMessage="Máximo de 12 parcelas"
+              type="number"
+              required="required"
+              label="Em quantas parcelas*"
+            />
+            <TextField id="mdr" type="number" required="required" label="Informe o percentual de MDR*" />
+          </form>
         </Simulator>
         <ResultSection>
           <ResultTitle>Você Receberá</ResultTitle>
           <ResultDays>
-            <li>
-              Amanhã: <strong>R$ 0,00</strong>
+            <li id="sei">
+              Amanhã: <strong data-one-value="0">R$ {VMasker.toMoney(state.receivables.oneDay)}</strong>
             </li>
             <li>
-              Em 15 dias: <strong>R$ 0,00</strong>
+              Em 15 dias: <strong data-fifteen-value="0">R$ {VMasker.toMoney(state.receivables.fifteenDays)}</strong>
             </li>
             <li>
-              Em 30 dias: <strong>R$ 0,00</strong>
+              Em 30 dias: <strong data-thirty-value="0">R$ {VMasker.toMoney(state.receivables.thirtyDays)}</strong>
             </li>
             <li>
-              Em 90 dias: <strong>R$ 0,00</strong>
+              Em 90 dias: <strong data-ninety-value="0">R$ {VMasker.toMoney(state.receivables.ninetyDays)}</strong>
             </li>
           </ResultDays>
         </ResultSection>
@@ -42,14 +50,61 @@ const Home = () => {
   );
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-  VMasker(document.getElementById('amount')).maskMoney({
-    precision: 2,
-    separator: ',',
-    delimiter: '.',
-    unit: 'R$',
-    zeroCents: true,
+const state = {
+  receivables: {
+    oneDay: 0,
+    fifteenDays: 0,
+    thirtyDays: 0,
+    ninetyDays: 0,
+  },
+};
+
+pubsub.subscribe('antecipationData', data => {
+  const { installmentAntecipation } = data;
+  installmentAntecipation.forEach(installment => {
+    if (Object.prototype.hasOwnProperty.call(installment, 1)) {
+      state.receivables.oneDay = parseFloat(installment[1]).toFixed(2);
+      document.querySelector('[data-one-value]').innerHTML = `R$ ${VMasker.toMoney(state.receivables.oneDay)}`;
+    }
+    if (Object.prototype.hasOwnProperty.call(installment, 15)) {
+      state.receivables.fifteenDays = parseFloat(installment[15]).toFixed(2);
+      document.querySelector('[data-fifteen-value]').innerHTML = `R$ ${VMasker.toMoney(state.receivables.fifteenDays)}`;
+    }
+    if (Object.prototype.hasOwnProperty.call(installment, 30)) {
+      state.receivables.thirtyDays = parseFloat(installment[30]).toFixed(2);
+      document.querySelector('[data-thirty-value]').innerHTML = `R$ ${VMasker.toMoney(state.receivables.thirtyDays)}`;
+    }
+    if (Object.prototype.hasOwnProperty.call(installment, 90)) {
+      state.receivables.ninetyDays = parseFloat(installment[90]).toFixed(2);
+      document.querySelector('[data-ninety-value]').innerHTML = `R$ ${VMasker.toMoney(state.receivables.ninetyDays)}`;
+    }
   });
+});
+
+const handleSubmit = form => {
+  const fields = Object.values(form.elements);
+  fields.forEach(function(element) {
+    element.addEventListener('blur', function() {
+      if (validateForm(form.elements)) {
+        const amountValue = currencyToFloat(document.getElementById('amount').value);
+        const installmentsValue = parseInt(document.getElementById('installments').value);
+        const mdrValue = parseInt(document.getElementById('mdr').value);
+        const result = getValueAntecipation({
+          amount: amountValue,
+          installmentQt: installmentsValue,
+          mdrRate: mdrValue,
+          daysAntecipation: [1, 15, 30, 90],
+        });
+        pubsub.publish('antecipationData', result);
+      }
+    });
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('form');
+  handleSubmit(form);
+  formatReal(document.getElementById('amount'));
 });
 
 const Container = ({ children }) => (
