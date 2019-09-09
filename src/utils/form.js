8e6bf3e4ce9b456b131c0da49f4f8f32c96a04af
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-09-09 16:27:20 -0300

    feat: cria funções utilitárias para validação de formulário e máscara de currency

---
+++ src/utils/form.js
@@ -0,0 +1,43 @@
+import VMasker from 'vanilla-masker';
+
+export const validateForm = form => {
+  for (const element in form) {
+    if (Object.prototype.hasOwnProperty.call(form, element)) {
+      if (!form[element].value) return false;
+      if (form[element].id === 'installments' && form[element].value > 12) {
+        form[element].value = 12;
+      }
+    }
+  }
+  return true;
+};
+
+const ocurrencesCount = (string, char) => {
+  const ocurrences = string.reduce((acc, current) => {
+    if (current === char) acc += 1;
+    return acc;
+  }, 0);
+  return ocurrences;
+};
+
+export const currencyToFloat = value => {
+  const moneyValue = value
+    .split('R$')
+    .join('')
+    .replace(',', '.')
+    .replace(' ', '');
+  if (ocurrencesCount(moneyValue.split(''), '.') > 1) {
+    return moneyValue.replace(moneyValue.charAt(moneyValue.indexOf('.')), '');
+  }
+  return moneyValue;
+};
+
+export const formatReal = value => {
+  VMasker(value).maskMoney({
+    precision: 2,
+    separator: ',',
+    delimiter: '.',
+    unit: 'R$',
+    zeroCents: false,
+  });
+};
